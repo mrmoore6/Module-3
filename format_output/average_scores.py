811c2b7adc9e30a1ac916b0a2228b609146d151c
mrmoore6 <mrmoore@dmacc.edu>
2020-09-12 13:42:10 -0500

    hello

---
+++ format_output/average_scores.py
@@ -0,0 +1,9 @@
+
+def average():
+    score1 = int(input("Enter 1st Test Score"))
+    score2 = int(input("Enter 2nd Test Score"))
+    score3 = int(input("Enter 3rd Test Score"))
+    return (score1 + score2 + score3) / 3
+
+
+
